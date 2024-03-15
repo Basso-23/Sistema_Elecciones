0660543233b8e541a94c20d3a60412dedd1070d5
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-03-14 22:14:09 -0500

    changes

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-const Inicio = () => {
-  return <div className=" flex flex-col gap-20">Inicio</div>;
-};
-
-export default Inicio;
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+const Inicio = () => {
+  return (
+    <main>
+      <div>Inicio</div>
+    </main>
+  );
+};
+
+export default Inicio;

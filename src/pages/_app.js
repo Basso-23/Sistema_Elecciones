1f033a744eaf3e8ec063e14477168def313ac33f
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-03-15 01:49:17 -0500

    changess

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,10 +6,13 @@ import { signOut } from "firebase/auth";
 import { auth } from "@/firebase/firebase";
 
 const App = ({ Component, pageProps, router }) => {
+  //* Almacena la el ID del usuario al iniciar sesion
   const [userState, setUserState] = useState();
+  //* Almacena los ID de los ADMIN
   const [adminID, setAdminID] = useState(
     process.env.ADMIN_ID ? JSON.parse(process.env.ADMIN_ID) : []
   );
+  //* Almacena los ID de los ACTIVISTAS
   const [activistaID, setActivistaID] = useState(
     process.env.ACTIVISTA_ID ? JSON.parse(process.env.ACTIVISTA_ID) : []
   );
